708310a1c8c44a3fd88cb2bff3812cc5fb7590ae
Mikko Heikkilä <Mikko@Jessi-MacBook-Pro.local>
2019-04-26 15:11:25 +0300

    Navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,14 +4,12 @@ import AniLink from "gatsby-plugin-transition-link/AniLink";
 export default () => (
     
     <nav>
-        <ul>    
-
+        <ul>
             <li><AniLink to="/">Etusivu (normal)</AniLink></li>
             <li><AniLink paintDrip to="/palvelut/" hex="#ed8d34">Palvelut (Paint)</AniLink></li>
             <li><AniLink fade to="/ihmiset/" >Ihmiset (Fade)</AniLink></li>
             <li><AniLink swipe direction="left" to="/meille-toihin/">Meille töihin (swipe)</AniLink></li>
             <li><AniLink cover to="/ota-yhteytta/" bg="#ed8d34">Ota yhteyttä (cover)</AniLink></li>
-
         </ul>
     </nav>
 
