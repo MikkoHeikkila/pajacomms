11d06dd841e60f339fbf30cf53c622e89b9af3c8
Mikko Heikkilä <Mikko@Jessi-MacBook-Pro.local>
2019-05-24 16:23:04 +0300

    Masonry

--- src/components/ReferencePreview/ReferencePreview.js
+++ src/components/ReferencePreview/ReferencePreview.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby";
+//import { Link } from "gatsby";
 import Img from "gatsby-image"
 import styles from "./ReferencePreview.module.css"
 
@@ -8,7 +8,7 @@ export default (props) => {
     return(
 
         <div className={`grid-item ${props.categoryslug} ${styles.singleReference}`}>
-            <Link to={'/palvelut/' + props.slug}>
+            {/*<Link to={'/palvelut/' + props.slug}>*/}
                 <div className={styles.imageContainer}>
                     <div className={`${props.categoryslug} reference-overlay ${styles.imageOverlay}`}></div>
                     <Img resolutions={props.image} />
@@ -16,7 +16,7 @@ export default (props) => {
                 </div>
                 <h4>{props.title}</h4>
                 <p className={styles.readMore}>Lue lisää</p>
-            </Link>
+            {/*</Link>*/}
         </div> 
 
     )
