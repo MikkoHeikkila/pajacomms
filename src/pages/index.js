9fb01896b0b0f819cd81e3c484e60e2aa62a307b
Mikko Heikkilä <Mikko@Jessi-MacBook-Pro.local>
2019-06-04 13:14:27 +0300

    AOS

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import SectionFullWidth from "../components/SectionFullWidth/SectionFullWidth";
 import SectionTxtImg from "../components/SectionTxtImg/SectionTxtImg";
 import SectionImgTxt from "../components/SectionImgTxt/SectionImgTxt";
 import SectionHero from "../components/SectionHero/SectionHero";
+import TextComponent from "../components/TextComponent/TextComponent";
 
 //Import animated images
 import SupermanContainer from "../components/animations/Superman"
@@ -13,6 +14,7 @@ import MuutosContainer from "../components/animations/Muutos"
 import PulaaContainer from "../components/animations/Pulaa"
 import PuuttuukoContainer from "../components/animations/Puuttuuko"
 import MedianakyvyysContainer from "../components/animations/Medianakyvyys"
+
 //Images to consts, to be passed as props to container components
 const Superman = <SupermanContainer />
 const Muutos = <MuutosContainer />
@@ -34,11 +36,14 @@ export default ({ data }) => {
                 <link rel="canonical" href="https://practical-austin-185700.netlify.com/" />
                 <html lang={currentPage.polylang_current_lang} />
             </Helmet>
-            <SectionHero content={currentPage.content} imageComponent={Muutos} />
+
+            <SectionHero content={currentPage.content} imageComponent={Muutos} aos="fade-up" />
             <SectionImgTxt content={currentPage.acf.rivi_2} imageComponent={Pulaa}/>
             <SectionFullWidth content={currentPage.acf.rivi_3} imageComponent={Puuttuuko} showBtn={true}/>
+            <TextComponent><h3>Reveal</h3></TextComponent>
             <SectionTxtImg content={currentPage.acf.rivi_4} imageComponent={Medianakyvyys}/>
             <SectionFullWidth content={currentPage.acf.rivi_5} imageComponent={Superman} showBtn={true} />
+
         </Layout>
 
     ) 
